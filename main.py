0430556085426060df643e303dfb214ffde31fd4
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-13 20:45:16 +0200

    Switch to python 3.11.15

---
+++ main.py
@@ -0,0 +1,11 @@
+# data
+import numpy as np
+import pandas as pd
+
+# machine learning
+import keras
+import ml_edu.experiment
+import ml_edu.results
+
+# data visualization
+import plotly.express as px
