55a3c5675274fcdaf6250506e62f3d56edffaef0
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-14 14:25:33 +0200

    Create folder for each project

---
+++ chicago-taxi/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+
+# machine learning
+import keras
+import ml_edu.experiment
+import ml_edu.results
+
+# data visualization
+import plotly.express as px
+
+# load data
+df = pd.read_csv("chicago_taxi_train.csv")
+
