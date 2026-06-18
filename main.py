912f26088eddd0eed7fae79afe7572c007bb9f8a
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-18 11:50:13 +0200

    Split data

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression 
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("melbourne/melb_data.csv")
+df = df.dropna(axis=0)
 
 # select target
 y = df['Price']
@@ -22,4 +25,14 @@ features = [
     ]
 
 X = df[features]
-print(X.head())
+
+# split the data
+train_X, val_X, train_y, val_y = train_test_split(X, y, test_size=0.2, random_state=0)
+# define a model
+model = LinearRegression()
+model.fit(X, y)
+
+print("estimated: ", model.predict(X.head()))
+print("actual: ", y.head())
+
+print("error: ", model.predict(X.head()) - y.head())
