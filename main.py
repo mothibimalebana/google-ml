162c339391065d6bfb890f6512fed2f7e85750f7
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-18 09:01:39 +0200

    Select features

--- main.py
+++ main.py
@@ -3,4 +3,23 @@ import pandas as pd
 df = pd.read_csv("melbourne/melb_data.csv")
 
 # select target
-y = df["Price"]
+y = df['Price']
+
+# select features
+features = [
+    'Rooms',
+    'Distance', 
+    'Postcode', 
+    'Bedroom2', 
+    'Bathroom', 
+    'Car', 
+    'Landsize', 
+    'BuildingArea', 
+    'YearBuilt', 
+    'Lattitude', 
+    'Longtitude', 
+    'Propertycount'
+    ]
+
+X = df[features]
+print(X.head())
