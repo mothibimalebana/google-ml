010b9f263a6191db39fc9c346b95c1894375e939
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-23 13:19:39 +0200

    Train a model with dropped columns to deal with missing values

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_absolute_error
 
+# read csv as a dataframe
 melbourne = pd.read_csv("melbourne/melb_data.csv")
 
-# select target
+# select target from dataframe
 y = melbourne['Price']
 
-# find features
-X = melbourne.drop('Price', axis=1) # drop price (target)
-
-# split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2, train_size=0.8)
+# select features
+X = melbourne.drop(['Price'], axis=1)     
 
 
-cols_with_missing_value = [col for col in X.columns if X[col].isnull().any()] # find list of columns w/ a missisng value
-X = X.drop(cols_with_missing_value, axis=1) # drop columns w/ a missing value
-X = X.select_dtypes(exclude=["object", "str"]) # for simplicity only take numberical
+# strategy 1: drop columns                                          
+cols_with_missing_value = [col for col in X.columns if X[col].isnull().any()]       
+reduced_X = X.drop(cols_with_missing_value, axis=1)                                         # drop columns with missing values
+reduced_X = reduced_X.select_dtypes(exclude=["object", "str"])
+
+# split data into training and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0)
+
+# score different methods used on dataset
+def score_dataset(X_train, X_test, y_test, y_train):
+    model = RandomForestRegressor(n_estimators=10, random_state=0)
+    melbourne_model = model.fit(X_train, y_train)
+    y_pred = melbourne_model.predict(X_test)
+    return mean_absolute_error(y_test, y_pred)
+
+print("drop columns: ", score_dataset(X_train, X_test, y_test, y_train))
