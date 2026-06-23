e323ebe8aa10891dfe990ef2961635076385c513
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-23 13:31:07 +0200

    Split first, apply strategy after

--- main.py
+++ main.py
@@ -13,14 +13,20 @@ y = melbourne['Price']
 # select features
 X = melbourne.drop(['Price'], axis=1)     
 
-
-# strategy 1: drop columns                                          
-cols_with_missing_value = [col for col in X.columns if X[col].isnull().any()]       
-reduced_X = X.drop(cols_with_missing_value, axis=1)                                         # drop columns with missing values
-reduced_X = reduced_X.select_dtypes(exclude=["object", "str"])
-
 # split data into training and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0)
+
+
+# strategy 1: drop columns                                          
+cols_with_missing_value = [col for col in X.columns if X[col].isnull().any()]   
+
+reduced_X_train = X_train.drop(cols_with_missing_value, axis=1)                                         
+reduced_X_train = reduced_X_train.select_dtypes(exclude=["object", "str"])
+
+
+reduced_X_test = X_test.drop(cols_with_missing_value, axis=1)                                         
+reduced_X_test = reduced_X_test.select_dtypes(exclude=["object", "str"])
+
 
 # score different methods used on dataset
 def score_dataset(X_train, X_test, y_test, y_train):
@@ -29,4 +35,4 @@ def score_dataset(X_train, X_test, y_test, y_train):
     y_pred = melbourne_model.predict(X_test)
     return mean_absolute_error(y_test, y_pred)
 
-print("drop columns: ", score_dataset(X_train, X_test, y_test, y_train))
+print("drop columns: ", score_dataset(reduced_X_train, reduced_X_test, y_test, y_train))
