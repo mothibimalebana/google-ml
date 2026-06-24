bc40ed9d1232ef45342ef457754536990c3e9ce3
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-24 06:25:33 +0200

    Add columns with low cardinality

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import mean_absolute_error
 
-# read csv as a dataframe
+# read the data
 melbourne = pd.read_csv("melbourne/melb_data.csv")
 
-# select target from dataframe
+# seperate the target from predictors
 y = melbourne['Price']
+X = melbourne.drop(['Price'], axis=1)
+X.copy()
 
-# select features
-X = melbourne.drop(['Price'], axis=1)     
-
-# split data into training and test
+# divide data into training and validation
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0)
 
+# drop columns with missing values (simplest approach)
+cols_with_missing_value = [col for col in X.columns if X[col].isnull().any()]
+X_train = X_train.drop(cols_with_missing_value, axis=1)
+X_test = X_test.drop(cols_with_missing_value, axis=1)
 
-# strategy 1: drop columns                                          
-cols_with_missing_value = [col for col in X.columns if X[col].isnull().any()]   
+# select columns with relatively low cardinality
+cols_with_low_cardinality = [col for col in X_train.columns if X_train[col].nunique() < 10 and (X_train[col].dtype == "str" or X_train[col].dtype == "object")]
 
-reduced_X_train = X_train.drop(cols_with_missing_value, axis=1)                                         
-reduced_X_train = reduced_X_train.select_dtypes(exclude=["object", "str"])
+# select numberical columns
+cols_with_numbers = [col for col in X_train.columns if (X_train[col].dtype == "int64" or X_train[col].dtype == "float64")]
 
+# keep selected columns only
+my_cols = cols_with_low_cardinality + cols_with_numbers
+X_train = X_train[my_cols].copy()
+X_test = X_test[my_cols].copy()
 
-reduced_X_test = X_test.drop(cols_with_missing_value, axis=1)                                         
-reduced_X_test = reduced_X_test.select_dtypes(exclude=["object", "str"])
-
-
-# score different methods used on dataset
-def score_dataset(X_train, X_test, y_test, y_train):
-    model = RandomForestRegressor(n_estimators=10, random_state=0)
-    melbourne_model = model.fit(X_train, y_train)
-    y_pred = melbourne_model.predict(X_test)
-    return mean_absolute_error(y_test, y_pred)
-
-print("drop columns: ", score_dataset(reduced_X_train, reduced_X_test, y_test, y_train))
+print(X_train.head())
